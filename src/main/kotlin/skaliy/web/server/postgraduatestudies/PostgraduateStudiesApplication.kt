1c671c099ab0fa84b171aa072c1b7aa6e5a4c98f
SkaliyD <skaliy.d@gmail.com>
2018-05-25 01:22:39 +0300

    Initial commit

---
+++ src/main/kotlin/skaliy/web/server/postgraduatestudies/PostgraduateStudiesApplication.kt
@@ -0,0 +1,11 @@
+package skaliy.web.server.postgraduatestudies
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PostgraduateStudiesApplication
+
+fun main(args: Array<String>) {
+    runApplication<PostgraduateStudiesApplication>(*args)
+}
